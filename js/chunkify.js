25d437ea1d613ab0c5bfb94e228008f81e679a29
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2021-11-24 21:27:07 -0300

    solução resolvida com sucesso

--- js/chunkify.js
+++ js/chunkify.js
@@ -1,9 +1,20 @@
 const chunkify = (inputArray, perChunk) => {
 
+ let array =[];
+ let qtdNumeros =inputArray;
+ let separador = perChunk;
+
+  for(var i =0; i< qtdNumeros.length;){
+      // array tu atribui em ti o valor no indice i até o i=i+separador  
+      array.push(qtdNumeros.slice(i,(i+=separador)));
+                               //i = i+separador 
+  }
+return array;
 }
 
+
 console.log("----------------------------");
-console.log("Chunks de 1 items", chunkify([1, 2, 3, 4], 1));
+console.log("Chunks de 1 items", chunkify([1, 2, 3,4], 1));
 console.log("----------------------------");
 console.log("Chunks de 2 items", chunkify([1, 2, 3, 4, 5, 6, 7], 2));
 console.log("----------------------------");
