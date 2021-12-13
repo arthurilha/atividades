60cb95990adad1d974cc4f246ffcd20ec6586234
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2021-12-13 16:40:00 -0300

    finalizando

--- CursoAngular/App1/src/app/progresso/progresso.component.ts
+++ CursoAngular/App1/src/app/progresso/progresso.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progresso',
   templateUrl: './progresso.component.html',
-  styleUrls: ['./progresso.component.css']
+  styleUrls: ['./progresso.component.s
+  css']
 })
 export class ProgressoComponent implements OnInit {
 
--- CursoAngular/App1/src/app/tentativas/tentativas.component.ts
+++ CursoAngular/App1/src/app/tentativas/tentativas.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
-  styleUrls: ['./tentativas.component.css']
+  styleUrls: ['./tentativas.component.scss']
 })
 export class TentativasComponent implements OnInit {
 
